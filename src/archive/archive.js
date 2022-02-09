a18304fe6461a0b4aba25af4675a2450fe562255
RAJARATHINAM-S <rajarathinams.mech@gmail.com>
2022-02-09 23:14:21 +0530

    added

--- src/archive/archive.js
+++ src/archive/archive.js
@@ -1,52 +1,52 @@
 import React, { useState, useEffect } from "react";
-import Addtask from "../addtask/addtask";
-import Navbar from "../navbar/navbar";
-import Sidebar from "../sidebar/sidebar";
+
 import "../notes/notes.css";
 import Tasknote from "../tasknote/tasknote";
 import axios from "axios";
 import "../archive/archive.css";
+import { useQuery } from "@apollo/client";
+import { getAllTasks } from "../Graphql";
 const Notes = () => {
-  const [data, setdata] = useState([]);
+  const { data } = useQuery(getAllTasks);
+  // const [data, setdata] = useState([]);
   const [fetch, setfetch] = useState(false);
-  useEffect(() => {
-    const fetchdata = async () => {
-      let result = await axios.get("/tasks?skip=0&limit=100", {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      });
-      console.log(result);
-      setdata(result.data);
-      console.log(data);
-    };
-    fetchdata();
-  }, [fetch]);
+  // useEffect(() => {
+  //   const fetchdata = async () => {
+  //     let result = await axios.get("/tasks?skip=0&limit=100", {
+  //       headers: {
+  //         Authorization: `Bearer ${localStorage.getItem("token")}`,
+  //       },
+  //     });
+  //     console.log(result);
+  //     setdata(result.data);
+  //     console.log(data);
+  //   };
+  //   fetchdata();
+  // }, [fetch]);
 
-  
   return (
     <>
       <div>
-       
         <div className="arhi">
-          {data
-            .filter((val) => {
-              return val?.isDeleted === false;
-            })
-            .filter((val) => {
-              return val?.isArchived === true;
-            })
-            .map(({ title, description, color, _id }, index) => {
-              return (
-                <Tasknote
-                  title={title}
-                  description={description}
-                  color={color}
-                  id={_id || index}
-                  setfetch={{ setfetch }}
-                />
-              );
-            })}
+          {data &&
+            data.tasks
+              .filter((val) => {
+                return val?.isDeleted === false;
+              })
+              .filter((val) => {
+                return val?.isArchived === true;
+              })
+              .map(({ title, description, color, _id }, index) => {
+                return (
+                  <Tasknote
+                    title={title}
+                    description={description}
+                    color={color}
+                    id={_id || index}
+                    setfetch={{ setfetch }}
+                  />
+                );
+              })}
         </div>
       </div>
     </>
